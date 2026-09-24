5df114131d2161eacb96127d3362dc3f5f60a5b2
tonovlaura <tonovlaura@gmail.com>
2026-09-24 13:31:43 +0300

    Mouse action tests in Playwright

---
+++ e2e/Chapter02/06_Mouse_Actions_Test.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('Mouse action tests in Playwright', async ({ page }) => {
+  // Navigate to the URL
+  await page.goto('https://github.com/tonovlaura');
+
+  // Left-click on the 'Repositories' link
+  await page.getByRole('link', { name: 'Repositories' }).click({button: 'left' });
+
+   // Middle-click on the 'Projects' link -> should open in new tab
+     // Wait for the new tab to open
+    const newTabPromise = page.context().waitForEvent('page');
+      // Middle click
+    await page.getByRole('link', { name: 'Projects' }).click({ button: 'middle',});
+    // Wait for the new tab to load
+    const newTab = await newTabPromise;
+    // checks if the new tab is opened
+    await newTab.waitForLoadState();
+
+  // Right-click on the 'Projects' link and open in new window
+    // waiting for new page to open, in PW there is no difference in new tab or page
+    const newWindowPromise = page.context().waitForEvent('page');
+    //Right click, but with command key (Meta) and Shift key to open in new window
+    await page.getByRole('link', { name: 'Projects' }).click({ modifiers: ['Meta', 'Shift'], });
+    // Wait for the new window to load
+    const newWindow = await newWindowPromise;
+    await newWindow.waitForLoadState();
+
+    await expect(newWindow).toHaveURL(/projects/);
+
+
+
+
+});
